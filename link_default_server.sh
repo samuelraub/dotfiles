e10238c89b9aacf31151be6502b6bac16ebe7704
Samuel Raub <samuel.raub@gmail.com>
2024-02-16 11:10:50 +0100

    fixup! link script

--- link_default_server.sh
+++ link_default_server.sh
@@ -1,3 +1,6 @@
+rm -rf ~/.zshrc
 ln -s ~/dotfiles/out/.zshrc/server/.zshrc ~/.zshrc
+rm -rf ~/.tmux.conf
 ln -s ~/dotfiles/out/.tmux.conf/default/.tmux.conf ~/.tmux.conf
+rm -rf ~/.vimrc
 ln -s ~/dotfiles/out/.vimrc/server/.vimrc ~/.vimrc
