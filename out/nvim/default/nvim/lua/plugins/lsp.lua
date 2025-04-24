e33cc070b89aa974e07723dd182ce3560b12e6f7
Samuel Raub <samuel.raub@gmail.com>
2025-04-24 10:19:40 +0200

    js lsp absolute imports

--- out/nvim/default/nvim/lua/plugins/lsp.lua
+++ out/nvim/default/nvim/lua/plugins/lsp.lua
@@ -7,6 +7,20 @@ return {
           mason = false,
           cmd = { vim.fn.expand("~/.rbenv/shims/ruby-lsp") },
         },
+        vtsls = {
+          settings = {
+            javascript = {
+              preferences = {
+                importModuleSpecifier = "non-relative",
+              },
+            },
+            typescript = {
+              preferences = {
+                importModuleSpecifier = "non-relative",
+              },
+            },
+          },
+        },
       },
     },
   },
